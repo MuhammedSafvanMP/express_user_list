e155981d029585267e73cc001ed02f57356671ad
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2024-04-03 11:41:44 +0530

    get only one user

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,17 @@
 const userList = require('../data/userData.json');
 
 
+// get all user data from userlist
+
 exports.getAllUsers = (request, response) => {
     return response.status(200).json(userList)
 }
+
+exports.getUserId = (request, response) => {
+    const userId = userList.find((userId) => userId.id === request.params.id)
+    if(!userId){
+         response.status(400).send("Bad request");
+    }
+     response.status(200).json(userId)
+}
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const userController = require('../controller/userController');
 
 
+// get all users data
 router.get('/', userController.getAllUsers);
+
+// get pasific user with help of id
+router.get('/:id', userController.getUserId);
 
 router.use('*', (request, response, next) => {
     response.status(404).send("404 Page not found");
