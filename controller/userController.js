ccd7222028b1824e1cfc3d71776d35acee0a4118
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2024-04-03 14:44:12 +0530

    edited user using put

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const userList = require('../data/userData.json');
-const crypto = require('crypto')
+const crypto = require('crypto');
+const { use } = require('../routes/userRoutes');
 
 
 // get all user data from userlist
@@ -13,11 +14,12 @@ exports.getAllUsers = (request, response) => {
 exports.getUserId = (request, response) => {
     const userId = userList.find((userId) => userId.id === request.params.id)
     if(!userId){
-         response.status(400).send("Bad request");
+         response.status(204).send(" No Content");
     }
-     response.status(200).json(userId)
+     response.status(302).json(userId)
 }
 
+// post creating new user in data
 
 exports.createUser = (request, response) => {
 
@@ -37,4 +39,29 @@ exports.createUser = (request, response) => {
     })
 
     return response.status(201).send("Created product");
+}
+
+
+exports.updateUser = (request, response) => {
+    const updateUser = userList.find((user) => user.id === request.params.id);
+
+    if(!updateUser){
+       return response.status(400).send(" Bad Request");   
+    }
+
+    const {name, email, userName} = request.body
+  
+    // assign new datas
+    
+    if(name)
+        updateUser.name = name;
+    if(email)
+        updateUser.email = email;
+    if(userName)
+        updateUser.userName = userName;
+    
+
+
+    return response.status(201).send("Modified user")
+
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,10 @@ router.get('/:id', userController.getUserId);
 // post create a user in user data
 router.post('/', userController.createUser);
 
+// put edit the user
+
+router.put('/:id', userController.updateUser)
+
 router.use('*', (request, response, next) => {
     response.status(404).send("404 Page not found");
 })
